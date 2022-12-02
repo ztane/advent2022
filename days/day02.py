1c9e57cad044a309f553ae4412aaa17331ef4a78
Antti Haapala <antti.haapala@anttipatterns.com>
2022-12-02 07:27:04 +0200

    RPS mostly in CoPilot.

--- days/day02.py
+++ days/day02.py
@@ -1,18 +1,98 @@
+from enum import Enum
+
 from helpers import *
 
 test_data = test_input("""
+A Y
+B X
+C Z
 """)
 
-test_case(1, test_data, ...)
-test_case(2, test_data, ...)
+test_case(1, test_data, 15)
+test_case(2, test_data, 12)
+
+
+# make Enum of rock, papers, scissors
+class RPS(Enum):
+    rock = 1
+    paper = 2
+    scissors = 3
+
+
+class Outcome(Enum):
+    lose = 0
+    tie = 3
+    win = 6
+
+
+rps_symbols = {
+    'A': RPS.rock,
+    'B': RPS.paper,
+    'C': RPS.scissors,
+    'X': RPS.rock,
+    'Y': RPS.paper,
+    'Z': RPS.scissors,
+}
+
+outcome_symbols = {
+    'X': Outcome.lose,
+    'Y': Outcome.tie,
+    'Z': Outcome.win,
+}
+
+result_table: Dict[Tuple[RPS, RPS], Outcome] = {
+    (RPS.rock, RPS.rock): Outcome.tie,
+    (RPS.rock, RPS.paper): Outcome.lose,
+    (RPS.rock, RPS.scissors): Outcome.win,
+    (RPS.paper, RPS.rock): Outcome.win,
+    (RPS.paper, RPS.paper): Outcome.tie,
+    (RPS.paper, RPS.scissors): Outcome.lose,
+    (RPS.scissors, RPS.rock): Outcome.lose,
+    (RPS.scissors, RPS.paper): Outcome.win,
+    (RPS.scissors, RPS.scissors): Outcome.tie,
+}
+
+# what I need to choose so that the outcome is what I want
+# (other, outcome) -> me, i.e. (rock, win) -> paper...
+choose_table: Dict[Tuple[RPS, Outcome], RPS] = {
+    (RPS.rock, Outcome.win): RPS.paper,
+    (RPS.rock, Outcome.tie): RPS.rock,
+    (RPS.rock, Outcome.lose): RPS.scissors,
+    (RPS.paper, Outcome.win): RPS.scissors,
+    (RPS.paper, Outcome.tie): RPS.paper,
+    (RPS.paper, Outcome.lose): RPS.rock,
+    (RPS.scissors, Outcome.win): RPS.rock,
+    (RPS.scissors, Outcome.tie): RPS.scissors,
+    (RPS.scissors, Outcome.lose): RPS.paper,
+}
 
 
 def part1(d: Input, ans: Answers) -> None:
-    ...
+    score = 0
+
+    for other, me in d.parsed_lines('<> <>'):
+        other = rps_symbols[other]
+        me = rps_symbols[me]
+
+        outcome = result_table[me, other]
+        score += outcome.value
+        score += me.value
+
+    ans.part1 = score
 
 
 def part2(d: Input, ans: Answers) -> None:
-    ...
+    score = 0
+
+    for other, outcome in d.parsed_lines('<> <>'):
+        other = rps_symbols[other]
+        outcome = outcome_symbols[outcome]
+
+        me = choose_table[other, outcome]
+        score += outcome.value
+        score += me.value
+
+    ans.part2 = score
 
 
-run([1], day=2, year=2022, submit=True)
+run([1, 2], day=2, year=2022)
